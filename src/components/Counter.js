63453186c42a678354cc35e7790a2fd95841d6b3
yeasinopu17 <yeasinopu@gmail.com>
2021-12-11 16:48:56 +0600

    com 243 with redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
+import { counterActions } from '../store';
 
 const Counter = () => {
     const counter = useSelector((state) => state.counter);
@@ -7,19 +8,22 @@ const Counter = () => {
     const dispatch = useDispatch();
 
     const incrementHandler = () => {
-        dispatch({ type: 'increment' });
+        dispatch(counterActions.increment());
     };
 
     const decrementHandler = () => {
-        dispatch({ type: 'decrement' });
+        dispatch(counterActions.decrement());
+
     };
 
     const increaseHandler = () => {
-        dispatch({ type: 'increase', amount: 5 });
+        dispatch(counterActions.increase(5));
+
     };
 
     const toggleCounterHandler = () => {
-        dispatch({ type: 'toggle' });
+        dispatch(counterActions.toggleCounter());
+
     };
 
     return (
